6b7cc08691f7ae88a91baf1eeeb686e8a157badb
Baha <abdelwahedbaha@gmail.com>
2018-10-30 22:43:50 +0100

    Update setup.rb

--- setup.rb
+++ setup.rb
@@ -3,4 +3,4 @@
 puts "Gems Instalation "
 system("gem install colorize && gem install nokogiri")
 puts "Tools Instalation"
-system("sudo apt-get install nmap && sudo apt-get install whatweb && sudo apt-get install host && sudo apt-get install dig && sudo apt-get install waw00f")
+system("sudo apt-get install nmap && sudo apt-get install whatweb && sudo apt-get install host && sudo apt-get install dig && sudo apt-get install wafw00f")
