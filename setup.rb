d5a55f984632b73e3439df3e4c43413558dbaf6f
F13END <elitearmyleader@gmail.com>
2018-11-03 12:55:12 +0300

    setup fixing error.
    Setup Nokogiri, wafw00f install error fixed and tested ubuntu 16.04 LTS
    sudo check.

--- setup.rb
+++ setup.rb
@@ -1,7 +1,16 @@
 #/usr/bin/ruby -w 
-
-puts "Gems Instalation "
-system("gem install colorize && gem install nokogiri")
+puts "If you can install this script should be root."
+system("sudo -s")
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+puts "Ruby Instalation"
+system("sudo apt-get install ruby-full")
+puts "Gems Instalation"
+system("sudo apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev")
+system("gem install nokogiri")
+system("gem install colorize")
 puts "Tools Instalation"
-system("sudo apt-get install nmap && sudo apt-get install whatweb && sudo apt-get install host && sudo apt-get install -y dnsutils
- && sudo apt-get install wafw00f")
+system("sudo apt-get install nmap && sudo apt-get install whatweb && sudo apt-get install host")
+system("sudo apt-get install -y dnsutils && sudo apt-get install wafw00f")
