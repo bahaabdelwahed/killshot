d58405b7165bde8a0350498fcd1a6b80977b9d4c
F13END <elitearmyleader@gmail.com>
2018-11-03 12:57:19 +0300

    fix command for bash to ruby

--- setup.rb
+++ setup.rb
@@ -1,12 +1,6 @@
 #/usr/bin/ruby -w 
 puts "If you can install this script should be root."
 system("sudo -s")
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-puts "Ruby Instalation"
-system("sudo apt-get install ruby-full")
 puts "Gems Instalation"
 system("sudo apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev")
 system("gem install nokogiri")
