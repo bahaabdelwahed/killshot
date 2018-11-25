b779ae7f9c424f771fcd9419b02b0ef7e01c54f1
Baha <abdelwahedbaha@gmail.com>
2018-11-25 22:38:13 +0100

    Update setup.rb

--- setup.rb
+++ setup.rb
@@ -1,10 +1,10 @@
 #/usr/bin/ruby -w 
 
 puts "Gems Instalation "
-system("gem install colorize && gem install net/http && gem install nokogiri && gem install shodan")
+system("gem install colorize && gem install shodan && gem install nokogiri")
 puts "Tools Instalation"
+system("sudo apt-get install nmap && sudo apt-get install whatweb && sudo apt-get install host ")
 system("git clone https://github.com/Und3rf10w/kali-anonsurf")
 system("apt-get install tor")
 system("cd kali-anonsurf")
 system("./installer.sh")
-system("sudo apt-get install nmap && sudo apt-get install whatweb && sudo apt-get install host ")
