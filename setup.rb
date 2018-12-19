716a8c9a69b1598bb2d2ee988ccdeceb85968b76
Baha <abdelwahedbaha@gmail.com>
2018-12-19 16:40:43 +0100

    Update setup.rb

--- setup.rb
+++ setup.rb
@@ -1,7 +1,9 @@
 #/usr/bin/ruby -w 
 
 puts "Gems Instalation "
-system("gem install colorize && gem install shodan && gem install nokogiri")
+system("gem install colorize && gem install shodan ")
+system("sudo apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev")
+system("gem install nokogiri")
 puts "Tools Instalation"
 system("sudo apt-get install nmap && sudo apt-get install whatweb && sudo apt-get install host ")
 system("git clone https://github.com/Und3rf10w/kali-anonsurf")
